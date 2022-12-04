aee0616e7ee4abb1c41aa277cd37aca3f30c8734
Munib Khan <kmunib048@gmail.com>
2022-12-04 16:05:33 +0500

    Add project files.

---
+++ JokesApp/Models/Joke.cs
@@ -0,0 +1,18 @@
+﻿namespace JokesApp.Models
+{
+    public class Joke
+    {
+        public int ID { get; set; }
+        public string JokeQuestion { get; set; }
+
+        public string JokeAnswer { get; set; }
+
+        public Joke()
+        {
+
+
+        }
+
+
+    }
+}
